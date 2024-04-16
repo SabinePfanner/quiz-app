7721c55472af672e9693dd36fa97c57f6289ccf7
Sabine Pfanner <pfanner.sabine@gmail.com>
2024-04-16 10:46:07 +0200

    add toggle functionalities to quiz-card

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const bookmark = document.querySelector('[data-js="bookmarks"]');
 
 bookmark.addEventListener("click", () => {
-  bookmark.innerHTML.toggle = `<path 
-    d="M200-120v-640q0-33 23.5-56.5T280-840h400q33 0 56.5 23.5T760-760v640L480-240 200-120Z"/>`;
-  bookmark.innerHTML.toggle = `<path 
-    d="M200-120v-640q0-33 23.5-56.5T280-840h400q33 0 56.5 23.5T760-760v640L480-240 200-120Zm80-122 200-86 200 86v-518H280v518Zm0-518h400-400Z"/>`;
+  bookmark.classList.toggle("fa-solid");
 });
+
+
+const showAnswer = document.querySelector('[data-js="show-answer"]');
+const answer = document.querySelector('[data-js="answer"]');
+
+showAnswer.addEventListener("click", () => {
+  answer.classList.toggle("hidden");
+  if (answer.classList.contains ("hidden")) {
+    showAnswer.textContent= "Antwort ansehen";
+  } else {
+    showAnswer.textContent="Antwort verstecken"
+  }
+})
