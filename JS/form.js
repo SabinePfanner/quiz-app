cd48126a0c30ac79fae1d283ff65ec14ef6b5af4
Sabine Pfanner <pfanner.sabine@gmail.com>
2024-04-21 18:54:25 +0200

    add text counter functionality to questionInput

--- JS/form.js
+++ JS/form.js
@@ -1,5 +1,10 @@
 const submitForm = document.querySelector('[data-js="form-new-card"]');
-const question = document.quer;
+const questionInput = document.querySelector('[data-js="quiz-question"]');
+const answerInput = document.querySelector('[data-js="quiz-answer"]');
+const tagListInput = document.querySelector('[data-js="tag-input"]');
+const charactersLeftElement = document.querySelector(
+  '[data-js="characters-left"]'
+);
 
 submitForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -17,17 +22,27 @@ submitForm.addEventListener("submit", (event) => {
   titleNC.classList.add("question");
   titleNC.textContent = event.target.question.value;
   const buttonNC = document.createElement("button");
-  titleNC.append(buttonNC);
+  newCard.append(buttonNC);
   buttonNC.classList.add("button");
   buttonNC.textContent = "Antwort ansehen";
   const answerNC = document.createElement("p");
   titleNC.append(answerNC);
-  answerNC.classList.add("answer", "hidden");
+  answerNC.classList.add("answer");
   answerNC.textContent = event.target.answer.value;
   const tagList = document.createElement("ul");
-  answer.append(tagList);
+  newCard.append(tagList);
   tagList.classList.add("tags");
   const tagsNC = document.createElement("li");
   tagList.append(tagsNC);
-  tagsNC.textContent = event.target.tagInput.value;
+  tagsNC.textContent = "#" + event.target.tagInput.value;
 });
+
+questionInput.addEventListener("input", () => {
+  charactersLeftElement.textContent = 150 - question.value.length + " Zeichen";
+});
+// answerInput.addEventListener("input", () => {
+//     charactersLeftElement.textContent = 150 - answer.value.length + " Zeichen";
+// });
+// tagListInput.addEventListener("input", () => {
+//     charactersLeftElement.textContent = 30 - tagInput.value.length + " Zeichen";
+// });
